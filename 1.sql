b51e435e6f90786eab8d4edf7b38c41935458a18
Chinmay Kotkar <102941004+Chinmay-03@users.noreply.github.com>
2023-11-17 00:06:17 +0530

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -125,7 +125,7 @@ Employees(empid);
 This assumes that the `Dept` table already exists, and you want to add a foreign key constraint 
 for the `Managerempid` column referencing the `empid` column in the `Employees` table.
 
-
+************************************
  1. DDL to implement the above schema:
 -- Table creation with primary and foreign key constraints
 CREATE TABLE Dept (
@@ -237,3 +237,107 @@ SELECT E.*FROM Employees E
 JOIN Works W ON E.empid = W.empid
 JOIN Project P ON W.Projectid = P.Projectid
 WHERE P.city = 'Pune'
+
+
+**********************************
+CREATE TABLE Dept (
+Deptno INT PRIMARY KEY, Name VARCHAR(255) NOT NULL, Location VARCHAR(255), Managerempid INT
+);
+CREATE TABLE Employees (
+empid INT PRIMARY KEY, name VARCHAR(255) NOT NULL, address VARCHAR(255), city VARCHAR(255), dob DATE, date_of_joining DATE, gender CHAR(1) CHECK (gender IN ('M', 'F')), salary DECIMAL(10,2), deptno INT
+);
+CREATE TABLE Project (
+Projectid INT PRIMARY KEY, title VARCHAR(255), city VARCHAR(255)
+);
+CREATE TABLE Works (
+empid INT, Projectid INT, total_hrs_worked INT, PRIMARY KEY (empid, Projectid)
+);
+CREATE TABLE Dependant (
+empid INT, name_of_dependant VARCHAR(255), age INT, relation VARCHAR(255), PRIMARY KEY (empid, name_of_dependant)
+);
+
+2.INSERT VALUES
+Insert data into DEPT:
+INSERT INTO Dept (Deptno, Name, Location, Managerempid)
+VALUES
+(1, 'HR Department', 'New York', 101), (2, 'IT Department', 'San Francisco', 102), (3, 'Testing Department', 'Pune', 103);
+Insert data into EMPLOYEES:
+INSERT INTO Employees (empid, name, address, city, dob, date_of_joining, gender, salary, deptno) VALUES
+(101, 'John Smith', '123 Main St', 'New York', '1980-01-15',
+'2020-02-01', 'M', 75000.00, 1), (102, 'Alice Johnson', '456 Tech St', 'San Francisco', '1990-05-20',
+'2019-05-10', 'F', 80000.00, 2), (103, 'Bob Williams', '789 Test St', 'Pune', '1985-09-08', '2021- 01-15', 'M', 70000.00, 3);
+Insert data into PROJECT:
+INSERT INTO Project (Projectid, title, city) VALUES
+(1, 'ERP project', 'New York'), (2, 'Software Development', 'San Francisco'), (3, 'Testing Automation', 'Pune');
+Insert data into Works:
+INSERT INTO Works (empid, Projectid, total_hrs_worked)
+VALUES
+(101, 1, 40), (102, 2, 35), (103, 3, 30);
+Insert data into Dependant:
+INSERT INTO Dependant (empid, name_of_dependant, age, relation) VALUES
+(101, 'Mary Smith', 10, 'Child'), (102, 'Charlie Johnson', 8, 'Child'), (103, 'Emma Williams', 12, 'Child');
+3.Alter commands
+Alter table dept
+ADD CONSTRAINT fk_manager FOREIGN KEY (Managerempid)
+REFERENCES Employees(empid);
+Alter table Employees
+ADD CONSTRAINT fk_dept FOREIGN KEY (deptno) REFERENCES
+Dept(Deptno);
+Alter table works
+ADD CONSTRAINT fk_emp FOREIGN KEY (empid) REFERENCES
+Employees(empid), ADD CONSTRAINT fk_project FOREIGN KEY (Projectid)
+REFERENCES Project(Projectid);
+Alter table Dependant
+ADD CONSTRAINT fk_emp_dependant FOREIGN KEY (empid)
+REFERENCES Employees(empid);
+4.SQL query to display all manager names:
+SELECT DISTINCT E.name
+FROM Employees E
+JOIN Dept D ON E.empid = D.Managerempid;
+5.SQL query to find the age of all employees:
+sql
+SELECT name, FLOOR(DATEDIFF(CURDATE(), dob) / 365) AS age
+FROM Employees;
+6. Display all employees in descending order of age:
+SELECT *
+FROM Employees
+ORDER BY dob DESC;
+7.SQL query to find the names of all employees who work for “ERP
+project”:
+SELECT E.name
+FROM Employees E
+JOIN Works W ON E.empid = W.empid
+JOIN Project P ON W.Projectid = P.Projectid
+WHERE P.title = 'ERP project';
+8.SQL query to find the names and cities of residence of all
+employees who work for “Banking project”:
+SELECT E.name, E.city
+FROM Employees E
+JOIN Works W ON E.empid = W.empid
+JOIN Project P ON W.Projectid = P.Projectid
+WHERE P.title = 'Banking project';
+9.SQL query to display the senior person of “Testing Department”:
+SELECT D.Name AS DeptName, E.name AS SeniorPerson, MAX(E.dob) AS SeniorDOB
+FROM Dept D
+JOIN Employees E ON D.Managerempid = E.empid
+WHERE D.Name = 'Testing Department' GROUP BY D.Name, E.name;
+10.SQL query to find the employees who are not having any project:
+SELECT E.name
+FROM Employees E
+LEFT JOIN Works W ON E.empid = W.empid
+WHERE W.empid IS NULL;
+
+11.Create a view containing the total number of employees whose
+project location is “Pune” and calculate total traveling allowance
+for all using the view (1000 Rs each):
+CREATE VIEW PuneEmployees AS
+SELECT E.empid
+FROM Employees E
+JOIN Works W ON E.empid = W.empid
+JOIN Project P ON W.Projectid = P.Projectid
+WHERE P.city = 'Pune';
+CREATE VIEW TravelingAllowance AS
+SELECT COUNT(empid) AS TotalEmployees, COUNT(empid) *
+1000 AS TotalAllowance
+FROM PuneEmployees; -- Query to use the view
+SELECT * FROM TravelingAllowance
